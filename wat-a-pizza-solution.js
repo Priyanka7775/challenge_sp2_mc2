ee545a1f59a56ccdf8ab7a6f832155429f59514f
PRIYANKA DEWANGAN <priyankadewangan7775@gmail.com>
2022-10-11 09:13:25 +0530

    completed

--- wat-a-pizza-solution.js
+++ wat-a-pizza-solution.js
@@ -75,20 +75,98 @@ const order = {
     ]
 };
 
+let orderAmount = new Array();
+
 //Write function to filter menu items by Category and sort them alphabetically
 
-//Write function to calculate the amount of each ordered item
+function category(menu,categoryType){
+
+    let filtered = menu.filter(item =>item.category === categoryType).sort((item1,item2) =>{
+        if(item1.name < item2.name)
+        return -1;
+        if(item1.name > item2.name)
+        return 1;
+        return 0;
+    })
+    return filtered;
+}
+
+console.log(category(menu,'Main Meal'))
+console.log(category(menu,'Starters'))
+console.log(category(menu,'Beverages'))
+
+//Write function to calculate thie amoufnt of each ordered item
+
+function calculateAmount(menu, order){
+
+    order.map (item => {
+    let orderAmounts={
+            "itemName" : item.name, 
+            "quantity" : item.quantity, 
+            "price" : item.price,
+            "category" : menu.filter(items => item.name===items.name)[0].category,
+            "amount" : item.quantity*item.price
+        }
+         orderAmount.push(orderAmounts)
+    })
+    return orderAmount;
+}
+
+let orderAmounts = calculateAmount(menu,order.items);
+console.log(orderAmount)
 
 //Write function to calculate the subtotal amount for the main meal ordered
+function calculateMainMealAmount(orderAmount){
+     intialValue = 0;
+    let mainMealAmount = 0;
+    orderAmount.filter(item => item.category == "Main Meal").reduce((total,item) => {
+       // total = total + item.amount
+        mainMealAmount = mainMealAmount + item.amount
+    },intialValue)
+
+    if(mainMealAmount > 48){
+        let cokePrice = orderAmount.filter(item => item.itemName == "Coke")[0].price
+        // console.log(cokePrice)
+        return mainMealAmount - cokePrice
+    }
+    return mainMealAmount; 
+}
+
+let mainMealAmount = calculateMainMealAmount(orderAmount);
+console.log(mainMealAmount)
 
 //Write function to find the totalAmount of the order based on the calculated mainMealAmount value
 
+let totalBillAmount = 0;
+function calculateTotalAmount(orderAmount, mainMealAmount){
+    orderAmount.filter(item => item.category != "Main Meal").reduce((total,item) => totalBillAmount += item.amount,intialValue )
+    return totalBillAmount + mainMealAmount;
+}
+
+let totalAmount1 = calculateTotalAmount(orderAmount,mainMealAmount)
+//console.log(totalAmount1)
+
 //Write function to calculate the final amount after discount
+function calculatedFinalAmount(totalBill, discount){
+    if(totalBill > 50)
+    return totalBill -= totalBill/100*discount;
+
+    return totalBill
+}
+
+
 
 //Write function to display the total amount 
+function displayTotalAmount(totalAmount){
+    console.log(`Total Amount payable: $ ${totalAmount1}`)
+}
 
 //Write function to display the final amount
+function displayFinalAmount(finalAmount){
+    console.log(`Final Amount payable after discount: $ ${finalAmount}`)
+}
 
 //Invoke  functions 
-
+displayTotalAmount(totalAmount1);
+displayFinalAmount(calculatedFinalAmount(totalAmount1,5));
 
